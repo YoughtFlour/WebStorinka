2636900b3932c8c23854cfc7ce5aa3b8273e1364
YoughtFlour <133506502+YoughtFlour@users.noreply.github.com>
2025-04-13 12:13:00 +0300

    Create reestr.php

---
+++ reestr.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Книги</title>
+</head>
+<h2>Введіть свої дані:</h2>
+<form action="reestr.php" method="POST">
+    <p>Введіть ім’я: <input type="text" name="firstname"></p>
+    <p>Введіть прізвище: <input type="text" name="lastname"></p>
+    <p>Введіть своє місто: <input type="text" name="city"></p>
+    <input type="submit" value="Відправити на сторінку reestr.php">
+</form>
+</body>
+</html>
